d4e75f38c5015c512d684d8aa31a04c49ebdb629
Sofiane <sidahmedsofiane29@gmail.com>
2018-07-19 16:06:59 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -74,6 +75,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((err) => {
          res.status(400).send();
          return;
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+        return;
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+            return;
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
